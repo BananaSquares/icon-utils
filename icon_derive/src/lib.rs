ff064bfa56f7c5206b79f2be9293b02892e2e364
Ewan Fox <ewan@ewanfox.com>
2023-02-21 19:46:57 -0500

    sort macro

--- icon_derive/src/lib.rs
+++ icon_derive/src/lib.rs
@@ -1,8 +1,8 @@
 use darling::FromDeriveInput;
-use proc_macro::{self,TokenStream};
+use proc_macro::{TokenStream};
 use quote::quote;
 use syn::{parse_macro_input,DeriveInput};
-
+use syn::parse::Parser;
 #[derive(FromDeriveInput, Default)]
 #[darling(attributes(params))]
 struct Params {
@@ -11,10 +11,11 @@ struct Params {
 
 #[proc_macro_derive(Transaction)]
 pub fn transaction(input: TokenStream) -> TokenStream {
-    let DeriveInput {ident, attrs, data,    ..} = parse_macro_input!(input as DeriveInput);
-    let fields = match &data {
+    let DeriveInput {ident, attrs, mut data,    ..} = parse_macro_input!(input as DeriveInput);
+    let fields = match &mut data {
         syn::Data::Struct(syn::DataStruct { fields: syn::Fields::Named(fields), ..}) => &fields.named,
         _ => panic!("Only structs with named fields are supported"),
+        
     };
     let paramType = fields.iter().find(|f| f.ident.as_ref().unwrap() == "params").unwrap().ty.clone();
     let output = quote! {
@@ -29,4 +30,31 @@ pub fn transaction(input: TokenStream) -> TokenStream {
         }
     };
     output.into()
+}
+#[proc_macro_attribute]
+pub fn sort(_args: TokenStream, input: TokenStream) -> TokenStream{
+    let mut ast = parse_macro_input!(input as syn::DeriveInput);
+    let ident = &ast.ident;
+    let data = &ast.data;
+    let fields = match &data {
+        syn::Data::Struct(syn::DataStruct { fields: syn::Fields::Named(fields), ..}) => &fields.named,
+        _ => panic!("Only structs with named fields are supported"),
+    };
+    let mut newFields: Vec<&syn::Field> = fields.iter().collect();
+    newFields.sort_by(|a, b| a.ident.as_ref().unwrap().cmp(&b.ident.as_ref().unwrap()));
+    let mut ast2 = ast.clone();
+    let mut data2 = &mut ast2.data;
+    match &mut data2 {
+        syn::Data::Struct(syn::DataStruct { fields: syn::Fields::Named(fields), ..}) => {
+            fields.named.clear();
+            for i in newFields {
+                fields.named.push(i.clone());
+            }
+        },
+        _ => panic!("Only structs with named fields are supported"),
+        
+    }
+    return quote! {
+        #ast2
+    }.into()
 }
